72d1f55cb11ecd7bec45ab75280d8ac6f837f5dc
github username <github email address>
2019-08-12 14:02:06 +0100

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,6 +11,9 @@ class EmailAddressParser
 	end
 
 	def parse
-		emails.split(/, | /).uniq
+		emails.split(/, | /).uniq #uniq returns a new array, getting rid of duplications
+		#split takes a string and breaks it into items in an array
+		#/ is used around the expression, and | is and 'or', so this says: break the string up into
+		#seperate items in an array, when you find a , and space, or just a space.
 	end
 end
